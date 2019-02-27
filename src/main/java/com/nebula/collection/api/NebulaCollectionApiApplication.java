9a7fe993383d542c9ade47c18cb7c278e528cfbc
yangsen <15763942407@163.com>
2019-02-27 23:50:53 +0800

    init

---
+++ src/main/java/com/nebula/collection/api/NebulaCollectionApiApplication.java
@@ -0,0 +1,7 @@
+package com.nebula.collection.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaCollectionApiApplication {
+}
